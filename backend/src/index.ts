095c9c4ca1dfd4324a52d8fe5620bf93b2c0ca6d
Andrew Walton <163466371+AndrewASUS@users.noreply.github.com>
2026-01-07 08:08:35 +1100

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,27 @@
+import express from "express"
+import cors from "cors"
+import { ENV } from "./config/env"
+import { clerkMiddleware } from '@clerk/express'
+
+
+const app = express()
+
+app.use(cors({ origin: ENV.FRONTEND_URL })) // Cross origin resorce sharing
+app.use(clerkMiddleware()) // Auth obj will be attached to he req
+app.use(express.json) // Parses JSON request bodies
+app.use(express.urlencoded({ extended: true })) // Parses from data (like HTML forms)
+
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "PERN-STORE API - Powered by PostgresSQL, Drizzle ORM & Clerk Auth",
+    endpoints: {
+      users: "/api/users",
+      products: "/api/products",
+      comments: "/api/comments"
+    }
+  })
+})
+
+
+app.listen(ENV.PORT, () => console.log("Server is running on PORT:", ENV.PORT))
